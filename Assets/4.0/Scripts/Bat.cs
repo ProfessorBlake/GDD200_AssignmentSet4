7089214085319e172d05c0c2ff0e9b7186d9effa
Blake <telefragent@gmail.com>
2020-03-19 12:36:40 -0400

    Initial commit

---
+++ Assets/4.0/Scripts/Bat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+	public Animator BatAnimator;
+
+	private void Update()
+	{
+		float moveDir = Input.GetAxis("Horizontal"); //Set in Edit -> Project Settings -> Input
+		if(moveDir != 0f)
+		{
+			transform.position += new Vector3(moveDir * Time.deltaTime, 0f, 0f);
+			BatAnimator.SetInteger("State", 1);
+		}
+		else
+		{
+			BatAnimator.SetInteger("State", 0);
+		}
+	}
+}
