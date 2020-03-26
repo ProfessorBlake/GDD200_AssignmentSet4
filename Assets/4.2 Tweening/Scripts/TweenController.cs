206dbb2f0c0efb2caf23aa9e7e2d973d09ac5aa0
Blake <telefragent@gmail.com>
2020-03-26 10:26:08 -0400

    4.2

---
+++ Assets/4.2 Tweening/Scripts/TweenController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenController : MonoBehaviour
+{
+	public Animator Animator;
+
+	private void Update()
+	{
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			Animator.SetInteger("state", 1);
+		}
+		else if (Input.GetKey(KeyCode.UpArrow))
+		{
+			Animator.SetInteger("state", 2);
+		}
+		else
+		{
+			Animator.SetInteger("state", 0);
+		}
+	}
+}
